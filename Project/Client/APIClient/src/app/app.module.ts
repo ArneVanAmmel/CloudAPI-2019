23fa0f1bb13bd573712b371ff1e5d4104f401bd4
ArneVanAmmel <lemmanavenra@gmail.com>
2019-03-23 17:24:12 +0100

    basic login page

--- Project/Client/APIClient/src/app/app.module.ts
+++ Project/Client/APIClient/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: "", component: LoginComponent},
+      {path: "home", component: HomeComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
